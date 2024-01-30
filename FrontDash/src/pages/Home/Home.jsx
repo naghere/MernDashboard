5a041e71dc4cd4aa3b60cd56c23a072a7ea68b77
João Henrique <joaohnsousa@gmail.com>
2024-01-30 19:04:59 -0300

    Mais Cards, Home Content bem preenchida

--- FrontDash/src/pages/Home/Home.jsx
+++ FrontDash/src/pages/Home/Home.jsx
@@ -1,22 +1,41 @@
+import "./Home.css";
+import Activity from "../../components/Activitys/Activity";
+import { LuArrowDownCircle } from "react-icons/lu";
 
-import './Home.css'
-import Activity from '../../components/Activitys/Activity';
-import { MdOutlinePendingActions } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div className='HomeContent'>
-      <h1>Home</h1>
+    <div className="HomeContent">
       <div className="list-activities">
-        <Activity number={2} status={"Pendente"} total={19}/>
-        <Activity number={4} status={"Em andamento"} total={19}/>
-        <Activity number={13} status={"Concluido"} total={19}/>
+        <Activity number={2} status={"Pendente"} total={19} />
+        <Activity number={4} status={"Em andamento"} total={19} />
+        <Activity number={13} status={"Concluido"} total={19} />
+        <Activity number={13} status={"Concluido"} total={19} />
       </div>
-      <div className="carrosel">
-        
+      <div className="Section-charts">
+        <div className="quantity-acts">
+          <div className="numRows">
+            <p>Quantidade de Atividades para essa semana</p>
+          </div>
+          <div className="circleQua">
+            <div className="numCircleQua">
+              5
+            </div>
+          </div>
+
+          <h4>Veja Mais...</h4>
+        </div>
+
+
+
+        <div className="content-charts">
+            <div className="chartModule">
+              
+            </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
